ad71a84e3181d8dbb20039ed0d986b787ec54385
Evelyn <145203060+EvelynOliveira1200@users.noreply.github.com>
2023-11-25 16:53:19 -0300

    Add files via upload

---
+++ jav.js/jav.js
@@ -0,0 +1,396 @@
+//let num1 = 20;
+
+//let num2 = 50;
+
+
+
+
+//soma = num1 + num2;
+
+
+
+
+//console.log(soma);
+
+//soma = num1 + num2;
+
+//mult= num1 * num2;
+
+//sub= num1 - num2;
+
+//div = num1 / num2;
+
+//rest = num1 % num2;
+
+
+
+
+//console.log(soma);
+
+//console.log(mult);
+
+//console.log(sub);
+
+//console.log(div);
+
+//console.log(rest);
+
+
+
+
+//let num1 = 50;
+
+//let num2 = 80;
+
+//let num3 = 90;
+
+//let num4 = 30;
+
+
+
+
+//media = (num1 + num2 + num3 +num4) / 4;
+
+
+
+
+//console.log(media);
+
+
+
+
+//let num1 = 61
+
+//let num2 = 1.64
+
+
+
+
+//imc = num1 / (num2 * num2)
+
+
+
+
+//console.log(imc)
+
+
+
+
+//let nome = "Evelyn";
+
+//let sobrenome = "Oliveira"
+
+
+
+
+//console.log(`meu nome é ${nome} ${sobrenome}`);
+
+
+
+
+//let idade = 18
+
+
+
+
+//if (idade <=10) {
+
+    //console.log("A pessoa é uma criança");
+
+//}else if(idade > 10 && idade <= 17){
+
+    //console.log("A pessoa é um adolescente")
+
+//}else if(idade >= 18 && idade <= 59){
+
+    //console.log("A pessoa é um adulto")
+
+//}
+
+
+
+
+//let peso = 61;
+
+//let altura = 1.64;
+
+//imc = (peso / (altura * altura)).toFixed(2)
+
+
+
+
+//if(imc < 18.5){
+
+  //  console.log("Abaixo do peso")
+
+//}else if(imc >= 18.5 && imc <= 25){
+
+   // console.log("Peso normal")
+
+//}else if(imc >= 25 && imc <= 30){
+
+   // console.log("Acima do peso")
+
+//}else if(imc >= 30 && imc <= 40){
+
+ //   console.log("Obeso")
+
+//}else{
+
+//    console.log("Obesidade grave")
+
+//}
+
+
+
+
+//function soma (x, y){
+
+  //  return x + y;
+
+//}
+
+//console.log(soma(2, 3));
+
+//console.log(soma(10, 25));
+
+//console.log(soma(50, 100));
+
+
+
+
+//function imc(peso, altura){
+
+//return (peso / (altura * altura)).toFixed(2);
+
+//}
+
+//console.log(imc(61, 1.64));
+
+//console.log(imc(61, 1.64));
+
+//console.log(imc(61, 1.64));
+
+//console.log(imc(61, 1.64));
+
+//console.log(imc(61, 1.64));
+
+//false
+
+
+
+
+//function saudacao(nome){
+
+//    return`Boa noite ${nome}`
+
+//}
+
+//console.log(saudacao("Maria"));
+
+//const raiz = function (n){
+
+  //  return n ** 0.5;
+
+//}
+
+//console.log(raiz(9));
+
+//const raiz1 = (n) =>{
+
+ //   return n ** 0.5;
+
+//}
+
+//console.log(raiz1(81));
+
+//const raiz2 = n => n ** 0.5;
+
+//console.log(raiz2(6561));
+
+//for (let i= 0; i<=10; i++){
+
+//   console.log(`O número 7 x ${i} = ${7*i}`);
+//}
+
+//let numero = 10;
+//numero = 20;
+//console.log(numero)
+
+//let numero;
+//numero = 20;
+//console.log(numero)
+
+//num1 = 10;
+//num2 = "10";
+//console.log(typeof num1);
+//console.log(typeof num2);
+//console.log(num1 === num2)
+
+//let num1 = 200;
+//let num2 = 50;
+//soma = num1 + num2;
+//sub = num1 - num2;
+//div = num1 / num2;
+//mult = num1 * num2;
+//rest = num1 % num2;
+
+//console.log(soma);
+//console.log(sub);
+//console.log(div);
+//console.log(mult);
+//console.log(rest);
+
+//let num1 = 50;
+//let num2 = 60;
+//let num3 = 80;
+//let num4 = 90;
+
+//let media = (num1 + num2 + num3 + num4) / 4;
+//console.log(media);
+
+
+//let idade = 70;
+
+//if (idade <=10){
+  //  console.log("Criança");
+//}
+//else if (idade >10 && idade<=17){
+ //   console.log("Adolescente");
+//}else if (idade >=18 && idade<=60){
+ //   console.log("Adulto");
+//}else{
+  //  console.log("Idoso")
+//}
+
+//let peso = 61;
+//let altura = 1.64;
+//imc = (peso / (altura * altura)).toFixed(2)
+
+//if(imc < 18.5){
+ //   console.log("Abaixo do peso")
+//}else if(imc >= 18.5 && imc <= 25){
+ //   console.log("Peso normal")
+//}else if(imc >= 25 && imc <= 30){
+ //   console.log("Acima do peso")
+//}else if(imc >= 30 && imc <= 40){
+ //   console.log("Obeso")
+//}else{
+  // console.log("Obesidade grave")
+//}
+
+//for (let i = 0; i <=10; i++){
+ //   console.log(i);
+//}
+//let i =0;
+//while (i <=10){
+ //   console.log(i);
+ //   i++;
+//}
+//let i =0;
+//do{
+ //   console.log(i);
+ //   i++;
+//}while (i < 10);
+
+//const frutas = ["maça", "Banana", "Goiaba", 10, 20, 1.45, true];
+
+//console.log(frutas);
+//frutas.push("uva");
+//console.log(frutas);
+//console.log(frutas.length)
+
+//const alunos = ["João", "Maria", "José"];
+//console.log(alunos.length);
+//console.log(alunos[1]);
+//console.log(alunos[0]);
+//const pessoa1 ={
+ //   nome: "Luis",
+   // sobrenome: "Augusto",
+    //idade: 45,
+    //altura: 1.75
+//}
+//const pessoa2 ={
+  //  nome: "Maria",
+    //sobrenome: "Ferreira",
+    //idade: 20,
+    //altura: 1.60
+//}
+//console.log(pessoa1.nome, pessoa1.sobrenome);
+//console.log(pessoa2.nome, pessoa2.sobrenome);
+
+//function soma(x, y){
+//    return x + y;
+//}
+//console.log(soma(10, 30));
+//console.log(soma(50, 130));
+
+//function imc (peso, altura){
+//return (peso / (altura * altura)).toFixed(2)
+//}
+//console.log(imc(61, 1.64))
+
+//function imc(peso, altura){
+//   let valor = peso / (altura * altura);
+
+//   if(valor < 18.5){
+//        return "Abaixo do pesso"
+//    }else if(imc >= 18.5 && imc <= 25){
+//        return "Peso normal"
+//}else if(imc >= 25 && imc <= 30){
+//        return "Acima do peso"
+//    }else if(imc >= 30 && imc <= 40){
+//        return "Obeso"
+//}else{
+//return "Obesidade grave"
+  // }
+//}
+//console.log(imc(1.64, 61))
+
+//let pagamento = "Á vista";
+
+//if(pagamento === "Á vista"){
+ //  console.log("10% de desconto")
+//}else if(pagamento === "Dinheiro" || pagamento === "PIX" || pagamento === "Débito"){
+ //   console.log("15% de desconto")
+//}else if(pagamento === "duas vezes"){
+ //   console.log("Preço normal sem juros")
+//}else{
+ //   console.log("Preço normal mas 10% de juros")
+//}
+
+//function pagamento(Formadepagamento){
+
+//if(Formadepagamento === "Á vista"){
+  //  return "10% de desconto"
+//}else if(Formadepagamento === "Dinheiro" || Formadepagamento === "PIX" || Formadepagamento === "Débito"){
+ //   return "15% de desconto"
+//}//else if(Formadepagamento === "duas vezes"){
+  //  return "Preço normal sem juros"
+//}else{
+ //    return "Preço normal mas 10% de juros"
+//}
+//}  
+//console.log(pagamento("Dinheiro"))
+
+function calcularImposto(salario){
+    let aliquota;
+    if(salario >0 && salario <=1100){
+        return aliquota = 0.05;
+    }else if(salario >=1100.01 && salario <=2500){
+        return aliquota = 0.1;
+    }else{
+        return aliquota = 0.15;
+    }
+}
+const valorSalario = 3000;
+const valorBeneficio = 400;
+
+const valorImposto = calcularImposto(valorSalario) * valorSalario;
+console.log(valorImposto);
+
+const salarioLiquido = valorSalario - valorImposto + valorBeneficio;
+console.log(salarioLiquido)
+
+
+
---
+++ jav.js/jav2.js
@@ -0,0 +1,4 @@
+//const nome = prompt("Digite seu nome completo");
+//document.body.innerHTML = `O meu nome completo é: ${nome}`;
+ 
+let botaoVermelho = document.getElementBy
